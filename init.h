a4ed349eb556a9e569b09f874c73cd28fdd7960d
Görkem / NT <67007893+Gork3m@users.noreply.github.com>
2020-12-27 16:49:59 +0300

    Create init.h

---
+++ init.h
@@ -0,0 +1,19 @@
+_1 = {
+    -- 27.12.2020
+    EquipItem = "f638f538",
+    Eject = "f9a70599",
+    SendVault = "f9f6af52",
+    TeamChange = "eeffc834",
+    Arrest = "c7e4c67a",
+    SpawnVehicle = "f31f9cd8",
+    RopeAttach = "a5731a18",
+    GetDonut = "d9d16004",
+    EatDonut = "c47c2f19",
+    EnterVehicle = "d1db4020",
+    LockCar = "c95c2009"
+}
+
+_2 = {
+
+
+}
