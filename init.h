2c90a320e0de62a197113665c2fc344030f287e6
Görkem / NT <67007893+Gork3m@users.noreply.github.com>
2020-12-28 11:39:20 +0300

    Update init.h

--- init.h
+++ init.h
@@ -23,8 +23,8 @@ _1 = {
     SpawnVehicle = "ee19de02",
     RopeAttach = "a5731a18", -- skip for now
     GetDonut = "d9d16004", -- skip for now
-    EatDonut = "c47c2f19",
-    EnterVehicle = "d1db4020",
-    LockCar = "c95c2009"
+    EatDonut = "c47c2f19", -- skip for now
+    EnterVehicle = "d24d715c",
+    LockCar = "b3763109"
 
 }
